9bc6828ecf2b3d052090a65eb248218123dc39a7
Draylar <samuelhimes@gmail.com>
2019-11-30 21:57:09 -0600

    1.15pre3/formatting/remove initializer

--- src/main/java/com/github/draylar/eiah/mixin/ESMixin.java
+++ src/main/java/com/github/draylar/eiah/mixin/ESMixin.java
@@ -11,31 +11,25 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MobEntity.class)
-public class ESMixin
-{
+public class ESMixin {
     @Inject(at = @At("HEAD"), method = "getPreferredEquipmentSlot", cancellable = true)
-    private static void getPreferredEquipmentSlot(ItemStack itemStack, CallbackInfoReturnable<EquipmentSlot> cir)
-    {
+    private static void getPreferredEquipmentSlot(ItemStack itemStack, CallbackInfoReturnable<EquipmentSlot> cir) {
         EquipmentSlot equipmentSlot;
         Item item = itemStack.getItem();
-        if (item != Blocks.CARVED_PUMPKIN.asItem() || ! (((BlockItem)item).getBlock() instanceof AbstractSkullBlock)) {
+
+        if (item != Blocks.CARVED_PUMPKIN.asItem() || !(((BlockItem) item).getBlock() instanceof AbstractSkullBlock)) {
             if (item instanceof ArmorItem) {
-                equipmentSlot = ((ArmorItem)item).getSlotType();
+                equipmentSlot = ((ArmorItem) item).getSlotType();
             } else if (item == Items.ELYTRA) {
                 equipmentSlot = EquipmentSlot.CHEST;
             } else {
                 equipmentSlot = item == Items.SHIELD ? EquipmentSlot.OFFHAND : EquipmentSlot.MAINHAND;
             }
-        }
-
-        else
-        {
+        } else {
             equipmentSlot = EquipmentSlot.HEAD;
         }
 
-
-        if (equipmentSlot == EquipmentSlot.MAINHAND)
-        {
+        if (equipmentSlot == EquipmentSlot.MAINHAND) {
             cir.setReturnValue(EquipmentSlot.HEAD);
         }
     }
